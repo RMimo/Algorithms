768ef3c0375b0e9410f62423ed0b2878d56b34a0
Ricardo Mimo <106038660+RMimo@users.noreply.github.com>
2024-09-28 09:47:09 +0200

    Create insertionSort1.py

---
+++ Sorting/insertionSort1.py
@@ -0,0 +1,13 @@
+# One common task for computers is to sort data. 
+# For example, people might want to see all their files on a computer sorted by size. 
+# Since sorting is a simple problem with many different possible solutions, 
+# it is often used to introduce the study of algorithms.
+
+# Insertion Sort is a simple and intuitive sorting algorithm. 
+# We will first start with a nearly sorted list.
+
+# Insert element into sorted list: 
+# Given a sorted list with an unsorted number e in the rightmost cell, 
+# how can we write some simple code to insert e into the array so that it remains sorted?
+
+# For example: arr = [1,2,4,5,3]. We can use brute-force to sort it.
