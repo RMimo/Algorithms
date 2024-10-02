3f776dd5359382734b5090d01e5f1bc6534157d7
Ricardo Mimo <106038660+RMimo@users.noreply.github.com>
2024-10-02 09:11:08 +0200

    Update marcsCakewalk.py

--- Greedy/marcsCakewalk.py
+++ Greedy/marcsCakewalk.py
@@ -1,1 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
 
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    miles = [cal*2**i for i, cal in enumerate(reversed(sorted(calorie)), 0)]
+    return sum(miles)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
